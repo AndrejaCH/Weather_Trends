25038e0685a1fa971cd03989a2760ad8ed5ffba6
Andreja Ho <sfandreja@gmail.com>
2021-03-17 18:41:11 -0700

    add SQL queries

---
+++ SQL_queries.sql
@@ -0,0 +1,20 @@
+
+--SQL QUERIES
+-- Select the city list
+SELECT * FROM city_list
+
+-- select only the cities that interests me
+SELECT * FROM city_data
+WHERE city = 'San Francisco';
+
+SELECT * FROM city_data
+WHERE city = 'Guangzhou';
+
+SELECT * FROM city_data
+WHERE city = 'Ljubljana';
+
+SELECT * FROM city_data
+WHERE city = 'Helsinki';
+
+-- select all data form global_data
+SELECT * FROM global_data
